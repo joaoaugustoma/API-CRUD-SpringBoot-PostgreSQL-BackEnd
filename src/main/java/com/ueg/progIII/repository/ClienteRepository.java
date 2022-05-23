bd1a6ec8553fe8bf96075ccf499faf4de01d89bf
joaoaugutoma <joaoaugustoananias@gmail.com>
2022-05-23 17:30:00 -0300

    Criação Repository: ClienteRepository.java

--- src/main/java/com/ueg/progIII/repository/ClienteRepository.java
+++ src/main/java/com/ueg/progIII/repository/ClienteRepository.java
@@ -2,6 +2,7 @@ package com.ueg.progIII.repository;
 
 import com.ueg.progIII.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,14 @@ import java.util.Optional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, String> {
+    List<Cliente> findByPrimeiroNome(String primeiroNome);
+
+    //JPQL
+    @Query("" +
+            "SELECT CASE WHEN COUNT(c) > 0 THEN " +
+            "TRUE ELSE FALSE END " +
+            "FROM Cliente c " +
+            "WHERE c.endereco = ?1"
+    )
+    Boolean exiteEndereco(String endereco);
 }
